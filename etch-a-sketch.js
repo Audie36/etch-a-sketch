81a77754c2634a89c69588e8ac091bcda6a111db
Audie Moradi <audiemoradi@gmail.com>
2021-08-10 09:57:27 -0500

    Reorganized code to make it more legible

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -32,6 +32,37 @@ let numberOfSquares = 16;
 
     addOptionButtons(options);
 
+// SELECT COLOR BUTTONS
+
+let button = document.getElementsByClassName('roundButton');
+
+    // Start with Black selected
+
+    bwButton.classList.add('selected');
+
+    // Selection - Function
+
+    let addSelectClass = function(){
+        // Call Remove Selection Function
+        removeSelectClass();
+        // Select Button
+        this.classList.add('selected');	
+    }
+
+    // Remove Selection - Function.  Remove selection
+
+    let removeSelectClass = function(){
+        for (let i =0; i < button.length; i++) {
+            button[i].classList.remove('selected')
+        }
+    }
+
+    // Run Selection Function - When button clicked
+    for (let i =0; i < button.length; i++) {
+        button[i].addEventListener("click",addSelectClass);
+    }
+
+
 // ETCH-A-SKETCH IMAGE
 
 
@@ -254,35 +285,7 @@ let numberOfSquares = 16;
 
         }
 
-// SELECT COLOR BUTTONS
 
-let button = document.getElementsByClassName('roundButton');
-
-    // Start with Black selected
-
-    bwButton.classList.add('selected');
-
-    // Selection - Function
-
-    let addSelectClass = function(){
-        // Call Remove Selection Function
-        removeSelectClass();
-        // Select Button
-        this.classList.add('selected');	
-    }
-
-    // Remove Selection - Function.  Remove selection
-
-    let removeSelectClass = function(){
-        for (let i =0; i < button.length; i++) {
-            button[i].classList.remove('selected')
-        }
-    }
-
-    // Run Selection Function - When button clicked
-    for (let i =0; i < button.length; i++) {
-        button[i].addEventListener("click",addSelectClass);
-    }
     
 
 
