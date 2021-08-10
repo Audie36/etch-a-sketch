cd75badcfe0c952bc9cbe85472fb8bfc012c8456
Audie Moradi <audiemoradi@gmail.com>
2021-08-10 09:40:46 -0500

    Organized CSS to make easier to read.  Added options to select color buttons.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -254,6 +254,24 @@ let numberOfSquares = 16;
 
         }
 
+// SELECT COLOR BUTTONS
+
+let button = document.getElementsByClassName('roundButton');
+
+var addSelectClass = function(){
+    removeSelectClass();
+    this.classList.add('selected');	
+}
+
+var removeSelectClass = function(){
+    for (var i =0; i < button.length; i++) {
+        button[i].classList.remove('selected')
+    }
+}
+
+for (var i =0; i < button.length; i++) {
+    button[i].addEventListener("click",addSelectClass);
+}
  
 
 
